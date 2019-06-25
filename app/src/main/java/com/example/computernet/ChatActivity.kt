095fc966e14a196feb41e18283ad790d1810b64e
Leon <978940343@qq.com>
2019-06-26 01:10:30 +0800

    chat demo finish

--- app/src/main/java/com/example/computernet/ChatActivity.kt
+++ app/src/main/java/com/example/computernet/ChatActivity.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 
 class ChatActivity : AppCompatActivity() {
 
@@ -11,7 +15,30 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val intent = Intent()
+        val toolbar: Toolbar = findViewById(R.id.toolbar_chat)
+        val editText: EditText = findViewById(R.id.edit_text)
+
+        setSupportActionBar(toolbar)
+
+        editText.setOnEditorActionListener { textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_DONE){
+                editText.setText("")
+            }
+            false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            android.R.id.home -> quit()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun quit(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     companion object {
--- app/src/main/java/com/example/computernet/ChatAdapter.kt
+++ app/src/main/java/com/example/computernet/ChatAdapter.kt
@@ -4,18 +4,18 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 
-class ChatAdapter(val mlist:List<String>): RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
+class ChatAdapter(val mList:List<String>): RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getItemCount(): Int = mlist.size
+    override fun getItemCount(): Int = mList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
+    class ViewHolder(v: View): RecyclerView.ViewHolder(v){
         val d=1
     }
 }
